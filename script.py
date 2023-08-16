1acf18f1f65680ae800c4f873511fbe0612cf70d
Shahzaib Ahmed <90938799+Shozy3@users.noreply.github.com>
2023-08-16 12:23:48 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,22 +1,42 @@
+<<<<<<< HEAD
+from __future__ import print_function
+=======
+>>>>>>> c09644bd6a72c5050a96d63be86da5c3c4fbd0b8
 import os
 import base64
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from email.message import EmailMessage
+
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from googleapiclient.discovery import build
-
 
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning, module='transformers', message=".*weights of PegasusForConditionalGeneration were not initialized.*")
+<<<<<<< HEAD
+=======
 
 import logging
 logging.basicConfig(level=logging.ERROR)
 
 
+import logging
+logging.basicConfig(level=logging.ERROR)
 
 # If modifying these SCOPES, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ['https://www.googleapis.com/auth/gmail.send', 'https://www.googleapis.com/auth/gmail.readonly']
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+tokenizer_path = os.path.join(script_dir, "local_pegasus_tokenizer")
+model_path = os.path.join(script_dir, "local_pegasus_model")
+
+tokenizer = PegasusTokenizer.from_pretrained(tokenizer_path)
+model = PegasusForConditionalGeneration.from_pretrained(model_path)
+
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -29,52 +49,63 @@ model = PegasusForConditionalGeneration.from_pretrained(model_path)
 
 def get_service():
     creds = None
-
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first time.
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json')
-
-    # If there are no (valid) credentials available, prompt the user to log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-
-        # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
-
     service = build('gmail', 'v1', credentials=creds)
     return service
 
+
 def main():
     service = get_service()
-
-    # Get the list of messages
-    results = service.users().messages().list(userId='me', maxResults=10).execute()
+    results = service.users().messages().list(userId='me', labelIds=['CATEGORY_PERSONAL'], maxResults=5).execute()
     messages = results.get('messages', [])
+    return_email = ""
 
     for message in messages:
         msg = service.users().messages().get(userId='me', id=message['id']).execute()
 
-        # Decode the email subject, sender, and body
         for header in msg['payload']['headers']:
             if header['name'] == 'Subject':
                 subject = header['value']
             elif header['name'] == 'From':
                 sender = header['value']
-
-        # Assuming the message is simple and has a text/plain part in 'parts'
         try:
             part = [part for part in msg['payload']['parts'] if part['mimeType'] == 'text/plain'][0]
             body = base64.urlsafe_b64decode(part['body']['data']).decode('utf-8')
         except:
+            continue
             body = "Could not decode body."
 
+<<<<<<< HEAD
+        tokens = tokenizer(body, truncation=True, padding="longest", return_tensors="pt")
+        summary_encoded = model.generate(**tokens)
+        summary_decoded = tokenizer.decode(summary_encoded[0])
+
+        return_email += "Subject: " + subject + '\n'
+        return_email += "Summary: " + summary_decoded + '\n\n'
+
+    message = EmailMessage()
+    message.set_content(return_email)
+    message['To'] = 'shahzaib@ualberta.ca'
+    message['From'] = 'shahzaib@ualberta.ca'
+    message['Subject'] = 'Test'
+    encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode('utf-8')
+    message_dict = {"raw": encoded_message}
+    try:
+        message = (service.users().messages().send(userId='me', body=message_dict).execute())
+        print('Message Sent!')
+    except HttpError as error:
+        print("An error occured: %s" % error)
+
+=======
         # print(f"From: {sender}\nSubject: {subject}\nBody: {body[:100]}...\n{'-'*50}")
 
         emails = {}
@@ -90,6 +121,7 @@ def main():
     
         print(subject)
         print(summary_decoded)
+>>>>>>> c09644bd6a72c5050a96d63be86da5c3c4fbd0b8
 
 if __name__ == '__main__':
     main()
